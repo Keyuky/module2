a6e4725f1f0a056d6134d3ec517623dbd21a5ca7
Keyuky <nguyenminhvu031202@gmail.com>
2022-12-06 21:22:46 +0700

    ver 1

---
+++ module 2/interest-calculator/src/com/codegym/Main.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double money = 0;
+        int month = 0;
+        double interestRate = 3.0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào số tiền gữi: ");
+        money = scanner.nextDouble();
+
+        System.out.println("Nhập vào tháng gữi: ");
+        month = scanner.nextInt();
+
+        double totalInterest = money * (interestRate/100)/12 * month;
+        System.out.println("Tổng lãi suất là: " + totalInterest);
+    }
+}
